d9a813226e2155cc4d01b88583fa605644a9cd9b
Schnugaf <espenonarheim@gmail.com>
2017-01-21 15:58:52 +0100

    Laget Musicplayer som velger sang og MusicEmitter som knyttes mot høytalere.

---
+++ Assets/Scripts/musicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicPlayer : MonoBehaviour {
+
+	public AudioClip[] songs;
+
+	public AudioClip oneSong;
+	public AudioClip twoSong;
+
+	// Use this for initialization
+	void Start () {
+		oneSong = songs[Random.Range(0, songs.Length - 1)];
+		twoSong = songs[Random.Range(0, songs.Length - 1)];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		oneSong = songs[Random.Range(0, songs.Length)];
+		twoSong = songs[Random.Range(0, songs.Length)];
+	}
+}
