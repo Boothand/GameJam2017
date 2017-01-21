7ac843298a0aaca74150804adeff98c52744993e
Axaka <martinvadseth@gmail.com>
2017-01-21 15:08:47 +0100

    Started making speaker script

---
+++ Assets/Scripts/Speaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour
+{
+
+	string fullSentence;
+	public string currentSentence;
+	public int currentOffence;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void getSentence()
+	{
+		fullSentence = TextGenerator
+	}
+}
