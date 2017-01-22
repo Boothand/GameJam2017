88c9b54bbe96b37f55751fe6163d204c07647b71
Axaka <martinvadseth@gmail.com>
2017-01-22 14:19:17 +0100

    Added simple main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject credits;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Club");
+	}
+
+	public void Credits()
+	{
+		credits.SetActive(!credits.active);
+	}
+}
