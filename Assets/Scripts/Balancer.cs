ef22cceddd69282997c603182ebd101016b47a1f
Schnugaf <espenonarheim@gmail.com>
2017-01-22 02:07:19 +0100

    Oppdatert for integrering.

---
+++ Assets/Scripts/Balancer.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class Balancer : ManagedPuppet
+{
+	[SerializeField] float balanceForce = 400f;
+	public Vector3 offCenter { get; private set; }
+	public Vector3 footCenter { get; private set; }
+
+	protected override void OnAwake()
+	{
+
+	}
+
+
+	void Update()
+	{
+		footCenter = (hierarchy.rFoot.position + hierarchy.lFoot.position) / 2;
+
+		Vector3 balanceCenter = footCenter;
+
+		balanceCenter.y = hierarchy.hips.position.y;
+		offCenter = balanceCenter - hierarchy.hips.position;
+
+		//if (offCenter.magnitude > 0.2f)
+		//{
+		//	return;
+		//}
+
+		hierarchy.hips.AddForce(Vector3.up * 4 /*+ (dir * 10) */ * balanceForce * Time.deltaTime);
+
+		if (true)//!mover.moving)
+		{
+			hierarchy.hips.AddForce((offCenter * 10) * balanceForce * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Hierarchy.cs
@@ -0,0 +1,24 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class Hierarchy : MonoBehaviour
+{
+	[SerializeField] public Rigidbody hips, spine;
+	[SerializeField] public Rigidbody rFoot, lFoot;
+	[SerializeField] public Rigidbody rThigh, lThigh;
+	[SerializeField] public Rigidbody rLeg, lLeg;
+	[SerializeField] public Rigidbody rUpperArm, lUpperArm;
+	[SerializeField] public Rigidbody rLowerArm, lLowerArm;
+	[SerializeField] public Rigidbody rHand, lHand;
+	[SerializeField] public Rigidbody head;
+
+	void Awake()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+}
