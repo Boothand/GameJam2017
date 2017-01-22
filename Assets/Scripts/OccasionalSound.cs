3972d806e7611d5cdf19aa21659c54da05f67016
Boothand <boothand@hotmail.com>
2017-01-22 13:59:22 +0100

    Occasion flushes or urinal sound

---
+++ Assets/Scripts/OccasionalSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OccasionalSound : MonoBehaviour
+{
+	AudioSource audioSource;
+	[SerializeField] AudioClip clip;
+	[SerializeField] float minTime = 10f;
+	[SerializeField] float maxTime = 30f;
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+		StartCoroutine(PlaySound());
+	}
+
+	IEnumerator PlaySound()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+			audioSource.PlayOneShot(clip);
+		}
+	}
+	
+	void Update()
+	{
+		
+	}
+}
