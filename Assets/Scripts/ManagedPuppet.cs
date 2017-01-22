84417c5b7f229d443ffdab74b58151d23e68a769
Boothand <boothand@hotmail.com>
2017-01-22 06:58:36 +0100

    Removed Rotator reference. Finally.

--- Assets/Scripts/ManagedPuppet.cs
+++ Assets/Scripts/ManagedPuppet.cs
@@ -6,7 +6,6 @@ public class ManagedPuppet : MonoBehaviour
 	[HideInInspector] public Hierarchy hierarchy;
 	protected Mover mover;
 	protected Balancer balancer;
-	protected Rotator rotator;
 	protected AbstractInput input;
 
 	protected virtual void Awake()
@@ -14,7 +13,6 @@ public class ManagedPuppet : MonoBehaviour
 		hierarchy = GetComponent<Hierarchy>();
 		mover = GetComponent<Mover>();
 		balancer = GetComponent<Balancer>();
-		rotator = GetComponent<Rotator>();
 		input = GetComponent<AbstractInput>();
 		OnAwake();
 	}
