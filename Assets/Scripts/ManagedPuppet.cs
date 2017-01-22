70bc32c4ba9950f97c1480762ec0dc7892783f1d
Schnugaf <espenonarheim@gmail.com>
2017-01-22 02:27:43 +0100

    BOX COLLIDERS!

--- Assets/Scripts/ManagedPuppet.cs
+++ Assets/Scripts/ManagedPuppet.cs
@@ -6,14 +6,13 @@ public class ManagedPuppet : MonoBehaviour
 	[HideInInspector] public Hierarchy hierarchy;
 	protected Mover mover;
 	protected Balancer balancer;
-	protected Rotator rotator;
+
 
 	protected virtual void Awake()
 	{
 		hierarchy = GetComponent<Hierarchy>();
 		mover = GetComponent<Mover>();
 		balancer = GetComponent<Balancer>();
-		rotator = GetComponent<Rotator>();
 		OnAwake();
 	}
 
